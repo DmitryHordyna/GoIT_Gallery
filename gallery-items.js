e13a8540c145e926cb4c242041156fc5781ab6ca
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2021-03-08 20:09:12 +0100

    d

--- gallery-items.js
+++ gallery-items.js
@@ -11,7 +11,7 @@ const galleryMarkup = images.map(el => {
     <li class="gallery__item">
     <a
      class="gallery__link"
-     href="${el.original}"
+   
 "
  >
     <img
@@ -23,10 +23,9 @@ const galleryMarkup = images.map(el => {
   </a>
 </li>
 `
+    // href="${el.original}"
 }).join('')
 galleryListRef.insertAdjacentHTML('beforeend', galleryMarkup)
-
-// === step 2
 
 galleryListRef.addEventListener('click',onClickModalOpen)
 function onClickModalOpen(e) {
@@ -58,7 +57,9 @@ function onClickBtn(e) {
   if (e.target.nodeName !== 'BUTTON') {
     return
   }
-  
+      modalRef.classList.remove('is-open')
+     contentImgRef.src = '';
+  contentImgRef.alt = '';
 }
 
 window.addEventListener('keyup', onKeyModalEsc)
@@ -71,54 +72,18 @@ function onKeyModalEsc(e) {
   contentImgRef.alt = '';
 }
 
-
-
-
-
-
-
-
-// const modalRef = document.querySelector('.lightbox')
-// const contentImgRef = document.querySelector('.lightbox__image')
-
-// const imgListRef=document.querySelector('.gallery__item')
-// galleryListRef.addEventListener('click', onClickModalOpen)
-
-// function onClickModalOpen({target})  {
-//   if (target.nodeName === 'IMG') {
-//     modalRef.classList.add('is-open')
-
-//     contentImgRef.src = target.src
-//     contentImgRef.alt=target.alt
-//   }
-// }
-
-// modalRef.addEventListener('click', onClickModalClose)
-
-// function onClickModalClose(e){
-//   if (e.currentTarget !== e.target) {
-    
-//   modalRef.classList.remove('is-open')
-//   }
-// }
-
-// // step 3
-
-// window.addEventListener('keyup', onKeyModalEsc)
-
-// function onKeyModalEsc(e) {
-//   if (e.key === 'Escape') {
-//      modalRef.classList.remove('is-open')
-//   }
-// }
-
-// window.addEventListener('keyup', onKeyModalLeftRight)
-
-// function onKeyModalLeftRight(e) {
-//   console.log(e.key);
+// window.addEventListener('keyup', onKeyLeftRight)
+// function onKeyLeftRight(e) {
 //   if (e.key === 'ArrowLeft') {
-//     console.log(imgListRef.childNodes[1].src);
-//   // console.log(contentImgRef.src)
-//   //   console.log(contentImgRef.alt)
+// console.log( );
+  // contentImgRef.alt
+//   }
+//   if (e.key === 'ArrowRight') {
+//         console.log('r');
 //   }
 // }
+
+
+
+
+
