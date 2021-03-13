a9104feeb710332f3e3f5976164b31f8707bfa54
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2021-03-13 12:42:39 +0100

    s

--- gallery-items.js
+++ gallery-items.js
@@ -1,69 +1,95 @@
 import images from './images.js';
 
-const galleryListRef = document.querySelector('.js-gallery')
-const modalRef = document.querySelector('.js-lightbox')
-const contentImgRef = document.querySelector('.lightbox__image')
+const galleryListRef = document.querySelector('.js-gallery');
+const modalRef = document.querySelector('.js-lightbox');
+const contentImgRef = document.querySelector('.lightbox__image');
 
 
-const galleryMarkup = images.map(el => {
-    return  `
+const galleryMarkup = images.map((el,index) => {
+  return `
     <li class="gallery__item">
     <a
      class="gallery__link"
        href="${el.original}"
-
-"
  >
     <img
+    id='scl'
     class="gallery__image"
     src ="${el.preview}"
     data-source="${el.original}"
      alt="${el.description}"
+     data-index='${index+1}';
     />
   </a>
 </li>
-`}).join('')
+`}).join('');
 
-galleryListRef.insertAdjacentHTML('beforeend', galleryMarkup)
-galleryListRef.addEventListener('click', onClickModalOpen)
-modalRef.addEventListener('click', onClickModalClose)
-modalRef.addEventListener('click', onClickBtnClose)
-window.addEventListener('keyup', onKeyModalEscClose)
+ galleryListRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+const allImgGalerry=document.querySelectorAll('.gallery__image')
+
+// console.log(allImgGalerry);
+
+
+galleryListRef.addEventListener('click', onClickModalOpen);
+modalRef.addEventListener('click', onClickModalClose);
+window.addEventListener('keyup', showNextPictureKey);
+window.addEventListener('keyup', onKeyModalEscClose);
+
 
 function onClickModalOpen(e) {
     e.preventDefault();
   if (e.target.nodeName !== "IMG") {
     return 
-  }
- closeAndCleanSrc()
+  };
+  closeAndCleanSrc();
   
-  let modalLink=e.target.dataset.source
- contentImgRef.src=modalLink
-
-}
+  let modalLink = e.target.dataset.source;
+  contentImgRef.src = modalLink;
+};
 function onClickModalClose(e) {
-  if (e.target.nodeName !== "DIV") {
+  if (e.target.nodeName !== "DIV" && e.target.nodeName !== 'BUTTON') {
     return
-  }
- closeAndCleanSrc()
-}
-function onClickBtnClose(e) {
-  if (e.target.nodeName !== 'BUTTON') {
-    return
-  }
-     closeAndCleanSrc()
-}
+  };
+  closeAndCleanSrc();
+};
 function onKeyModalEscClose(e) {
   if (e.key !== 'Escape') {
     return
+  };
+  closeAndCleanSrc();
+};
+
+function showNextPictureKey(e) {
+  if (e.key === 'ArrowLeft') {
+    for (let i = 0; i < allImgGalerry.length; i += 1) {
+      if (allImgGalerry[i].dataset.source === contentImgRef.src) {
+        contentImgRef.src = allImgGalerry[i - 1].dataset.source;
+      } else (contentImgRef.dataset.index === 1){
+        
+      }
+    };
+  };
+  if (e.key === 'ArrowRight') {
+  //   for (let i = 0; i < allImgGalerry.length; i += 1) {
+  //     if (allImgGalerry[i].dataset.source === contentImgRef.src) {
+  //       contentImgRef.src=allImgGalerry[i + 1].dataset.source;
+       
+      
+  //     }
+  //   }
   }
-closeAndCleanSrc()
 }
+
 function closeAndCleanSrc() {
-       modalRef.classList.toggle('is-open')
-     contentImgRef.src = '';
+  modalRef.classList.toggle('is-open');
+  contentImgRef.src = '';
   contentImgRef.alt = '';
-}
+};
+
+
+
 
 
 
